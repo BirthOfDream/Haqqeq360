3fda9bb56b0e8257731526f127823a665d54628b
Blue-chocolate <haasklany@gmail.com>
2025-11-30 10:26:58 +0200

    Parternsapi

--- app/Http/Controllers/Api/PartnersControllers/PartnersController.php
+++ app/Http/Controllers/Api/PartnersControllers/PartnersController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\PartnersControllers;
+namespace App\Http\Controllers\Api\PartnersController;
 
 use App\Http\Controllers\Controller;
 use App\Models\Partner;
